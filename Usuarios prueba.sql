3da9093c5a9271fa65a5eef38152b8d5f2915413
danielviquezsolano <daviquez@estudiantec.cr>
2025-10-08 22:05:24 -0600

    Creación de usuarios prueba para BD

---
+++ Usuarios prueba.sql
@@ -0,0 +1,12 @@
+USE Tarea2BD
+GO
+INSERT INTO Usuario (username, password)
+VALUES ('Prueba', '1234')
+INSERT INTO Usuario (username, password)
+VALUES ('Prueba2', '1234')
+INSERT INTO Usuario (username, password)
+VALUES ('Prueba3', '1234')
+INSERT INTO Usuario (username, password)
+VALUES ('Prueba4', '1234')
+
+SELECT * FROM Usuario
